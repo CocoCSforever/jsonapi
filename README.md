8132b71c7aea54a0cf88b043e81f812e8a04292e
CocoCSforever <yijia.ma18@gmail.com>
2023-09-21 17:14:14 -0700

    Created setup.py for packaging and distribution, Organized project into  and  directories, Created source distribution and wheel, Updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # jsonapi
+
+This Python package provides a customized encoder and decoder for specific data structures like `complex` and `range` that are not directly supported by the standard JSON module.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+  - [Encode the data](#Encode the data)
+  - [Decode the JSON data](#Decode the JSON data)
+- [Contributing](#contributing)
+- [License](#license)
+
+
+## Installation
+
+You can install the package using `pip`:
+
+```bash
+pip install jsonapi
+```
+
+
+## Usage
+
+from jsonapi import YEncoder, YDecoder, dumps, loads
+
+# Create an object to encode
+data = {
+    'complex_number': complex(1, 2),
+    'range_object': range(1, 10, 2)
+}
+
+# Encode the data
+encoded_data = YEncoder().encode(data)
+print("Encoded Data:", encoded_data)
+
+encoded_data = dumps(data, cls=YEncoder)
+print("Encoded Data:", encoded_data)
+
+# Sample JSON data (encoded using YEncoder)
+json_data = '{"complex_number": {"real": 1.0, "imag": 2.0, "__extended_json_type__": "complex"}, "range_object": {"start": 1, "stop": 10, "step": 2, "__extended_json_type__": "range"}}'
+
+# Decode the JSON data
+decoded_data = YDecoder().decode(json_data)
+print("Decoded Data:", decoded_data)
+
+decoded_data = loads(json_data, cls=YDecoder)
+print("Decoded Data:", decoded_data)
+
+## Contributing
+Contributions are welcome!
+
+## License
+None
---
+++ src/jsonapi/jsonapi.py
@@ -0,0 +1,55 @@
+# extend the json library to support complex and range objects
+import json
+
+
+class YEncoder(json.JSONEncoder):
+    def default(self, obj):
+        name = type(obj).__name__
+        try:
+            encoder = getattr(self, f"encode_{name}")
+        except AttributeError():
+            return super().default(obj)
+        else:
+            encoded = encoder(obj)
+            encoded["__extended_json_type__"] = name
+            return encoded
+    
+    def encode_complex(self, c):
+        return {"real": c.real, "imag": c.imag}
+    
+    def encode_range(self, r):
+        return {"start": r.start, "stop": r.stop, "step": r.step}
+    
+    
+class YDecoder(json.JSONDecoder):
+    def __init__(self, **kwargs):
+        kwargs["object_hook"] = self.object_hook
+        super().__init__(**kwargs)
+    
+    def object_hook(self, obj):
+        try:
+            # basic value doesn't have key named "__extended_json_type__", and will get KeyError
+            name = obj["__extended_json_type__"]
+            decoder = getattr(self, f"decode_{name}")
+        except (KeyError, AttributeError):
+            return obj
+        else:
+            return decoder(obj)
+    
+    def decode_complex(self, obj):
+        real = obj["real"]
+        imag = obj["imag"]
+        return complex(real, imag)
+    
+    def decode_range(self, obj):
+        start = obj["start"]
+        stop = obj["stop"]
+        step = obj["step"]
+        return range(start, stop, step)
+    
+
+def dumps(obj, cls=YEncoder, *args, **kwargs):
+    return json.dumps(obj, cls=cls, *args, **kwargs)
+
+def loads(s, cls=YDecoder, *args, **kwargs):
+    return json.loads(s, cls=cls, *args, **kwargs)
